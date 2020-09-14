c9e2b7cc46795dc816591de6d7a309c34e9c3cb1
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-09-14 20:09:52 +0300

    Display the profile

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 const github = new Github();
+const ui = new UI();
 
 const UIsearchUserName = document.querySelector('#searchUser');
 
 UIsearchUserName.addEventListener('keyup', e => {
-  const username = e.target.value;
+  let username = e.target.value;
 
   if(username !== '') {
     github.getUser(username)
           .then( data => {
-            console.log(data)
+            if(data.profile.message === 'Not Found') {
+              // Show alert
+              console.log('Unable to fetch data');
+            }else {
+              // Show profile
+              ui.showProfile(data.profile);
+            }
           })
           .catch(err => console.log(err));
   }else {
     username = '';
+    console.log('Empty')
   }
 })
